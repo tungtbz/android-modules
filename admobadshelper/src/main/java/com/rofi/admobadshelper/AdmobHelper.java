34e92a900a9904793d8c73dd7f2d6a7a86badd63
tungtbz <tungt@bravezone.com>
2024-10-04 10:10:31 +0700

    reload mrec if failed to load

--- admobadshelper/src/main/java/com/rofi/admobadshelper/AdmobHelper.java
+++ admobadshelper/src/main/java/com/rofi/admobadshelper/AdmobHelper.java
@@ -515,6 +515,12 @@ public class AdmobHelper {
             public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
                 super.onAdFailedToLoad(loadAdError);
                 Log.d(TAG, "MREC onAdFailedToLoad" + "\nloadAdError: " + loadAdError.getMessage());
+
+                // Tải lại quảng cáo sau 30 giây
+                mrecAdView.postDelayed(() -> {
+                    AdRequest adRequest = new AdRequest.Builder().build();
+                    mrecAdView.loadAd(adRequest);
+                }, 30000);  // 30 giây
             }
         });
 
